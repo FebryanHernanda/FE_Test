6c6100bef4f4ac2eae39b58d13e0330b58d7d550
FebryanHernanda <febryanhernanda@gmail.com>
2026-01-16 21:51:40 +0700

    chore: re-setup base project config

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { CssBaseline } from '@mui/material'
 
-import App from '@/app/App'
-import { queryClient } from '@/app/queryClient'
+
+import App from '@/App'
+import { AppProviders } from '@/components/common/providers'
 
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <CssBaseline />
+    <AppProviders>
       <App />
-    </QueryClientProvider>
+    </AppProviders>
   </React.StrictMode>
 )
